b53a082de077d2f6d6fb045855384f43a1ff8183
Sandesh4141 <hackersp41@gmail.com>
2026-07-31 01:37:14 +0530

    added navigation

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,49 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from "../features/auth/screens/LoginScreen";
+
+import { RootStackParamList } from "./types/types";
+import OrdersScreen from "../features/orders/screens/OrdersScreen";
+import OrdersDetailsScreen from "../features/orders/screens/OrdersDetailsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerBackTitle: "Back",
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: "Login",
+
+          }}
+        />
+
+        <Stack.Screen
+          name="Orders"
+          component={OrdersScreen}
+          options={{
+            title: "Orders",
+          }}
+        />
+
+        <Stack.Screen
+          name="OrderDetails"
+          component={OrdersDetailsScreen}
+          options={{
+            title: "Order Details",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/navigation/types/types.ts
@@ -0,0 +1,7 @@
+export type RootStackParamList = {
+  Login: undefined;
+  Orders: undefined;
+//   OrderDetails: {
+//     orderId: string;
+//   };
+};
