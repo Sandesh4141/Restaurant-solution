6c14e4236ddb2d2e854796308a0a400c9402cfed
Sandesh4141 <hackersp41@gmail.com>
2026-07-31 04:54:39 +0530

    reset code removed

--- App.tsx
+++ App.tsx
@@ -1,14 +1,27 @@
+import ToastProvider from "react-native-toast-message";
 import { View } from "react-native";
 import { AppNavigator } from "./src/navigation/AppNavigator";
 import { OrdersProvider } from "./src/features/orders/context/orderContext";
-
+import { Toast, toastConfig } from "./src/components/Toast";
+import { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function App() {
+
+  //  useEffect(() => {
+  //   async function reset() {
+  //     await AsyncStorage.removeItem("@restaurant_orders");
+  //     console.log("Orders reset");
+  //   }
+
+  //   reset();
+  // }, []);
+
   return (
     <View style={{ flex: 1 }}>
-      
       <OrdersProvider>
-        <AppNavigator />;
+        <AppNavigator />
       </OrdersProvider>
+      <Toast config={toastConfig} />
     </View>
   );
 }
